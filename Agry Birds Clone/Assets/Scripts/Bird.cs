0442939071ce16c42a0b23d126b7c2e9666abce9
pavloskii <pavlo.igor@gmail.com>
2021-08-13 00:33:05 +0200

    Added crates and monsters

---
+++ Agry Birds Clone/Assets/Scripts/Bird.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField]
+    float _launchForce = 500;
+
+    Vector2 _startPosition;
+    Rigidbody2D _rigidbody2D;
+    SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = _rigidbody2D.position;
+        _rigidbody2D.isKinematic = true;
+    }
+
+    void OnMouseDown()
+    {
+        _spriteRenderer.color = Color.red;
+    }
+
+    void OnMouseUp()
+    {
+        Vector2 currentPosition = _rigidbody2D.position;
+        Vector2 direction = _startPosition - currentPosition;
+        direction.Normalize();
+
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(direction * _launchForce);
+
+        _spriteRenderer.color = Color.white;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 desiredPosition = mousePosition;
+        if (desiredPosition.x > _startPosition.x)
+        {
+            desiredPosition.x = _startPosition.x;
+        }
+
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(ResetAfterDelay());
+    }
+
+    IEnumerator ResetAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+
+        _rigidbody2D.position = _startPosition;
+        _rigidbody2D.isKinematic = true;
+        _rigidbody2D.velocity = Vector2.zero;
+    }
+}
