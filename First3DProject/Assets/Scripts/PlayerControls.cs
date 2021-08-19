7acafc8acba3a729af96ddfc7707c9acbe8387b3
pavloskii <pavlo.igor@gmail.com>
2021-08-20 00:50:04 +0200

    PLayer looking in the moving direction

--- First3DProject/Assets/Scripts/PlayerControls.cs
+++ First3DProject/Assets/Scripts/PlayerControls.cs
@@ -5,24 +5,46 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     public float MoveSpeed = 1.5f;
+    public GameObject PlayerDragon;
 
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(MoveSpeed * Time.deltaTime * Vector3.left);
+            PlayerDragon.transform.localRotation = Quaternion.Euler(0, 270, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(MoveSpeed * Time.deltaTime * Vector3.right);
+            PlayerDragon.transform.localRotation = Quaternion.Euler(0, 90, 0);
+
         }
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(MoveSpeed * Time.deltaTime * Vector3.forward);
+            PlayerDragon.transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(MoveSpeed * Time.deltaTime * Vector3.back);
+            PlayerDragon.transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            PlayerDragon.transform.localRotation = Quaternion.Euler(0, 45, 0);
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+        {
+            PlayerDragon.transform.localRotation = Quaternion.Euler(0, 135, 0);
+        }
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            PlayerDragon.transform.localRotation = Quaternion.Euler(0, 315, 0);
+        }
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            PlayerDragon.transform.localRotation = Quaternion.Euler(0, 225, 0);
         }
     }
 }
