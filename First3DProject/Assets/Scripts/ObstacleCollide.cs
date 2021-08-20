38a2087e0d2fc5ddcaf1e40f75a1d8eeed627297
pavloskii <pavlo.igor@gmail.com>
2021-08-21 00:03:36 +0200

    Obstacles created

---
+++ First3DProject/Assets/Scripts/ObstacleCollide.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleCollide : MonoBehaviour
+{
+    public GameObject HurtText;
+    public GameObject FadeOut;
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player.GetComponent<PlayerControls>().enabled = false;
+            HurtText.SetActive(true);
+            FadeOut.SetActive(true);
+            StartCoroutine(RespawningLevel());
+            return;
+        }
+    }
+    IEnumerator RespawningLevel()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(1);
+    }
+}
