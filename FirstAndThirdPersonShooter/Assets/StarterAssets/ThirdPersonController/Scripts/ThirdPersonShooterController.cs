103199429ef30cf8c9906548216c6b37014f7d62
pavloskii <pavlo.igor@gmail.com>
2021-09-05 21:41:26 +0200

    Added Ray point to see where the cursor is hitting

--- FirstAndThirdPersonShooter/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
+++ FirstAndThirdPersonShooter/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
@@ -1,27 +1,57 @@
 using UnityEngine;
 using Cinemachine;
 using StarterAssets;
-using System;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
     [SerializeField]
     private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField]
+    private float normalSensitivity;
+    [SerializeField]
+    private float aimSensitivity;
+    [SerializeField]
+    private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField]
+    private Transform debugTransform;
 
+    private ThirdPersonController thirdPersonController;
     private StarterAssetsInputs starterAssetsInputs;
 
     private void Awake()
     {
+        thirdPersonController = GetComponent<ThirdPersonController>();
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
     }
 
     private void Update()
     {
+
+
         ToggleAim(starterAssetsInputs.aim);
     }
 
     private void ToggleAim(bool aim)
     {
+        Vector3 mouseWorldPosition = Vector3.zero;
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+        }
+
         aimVirtualCamera.gameObject.SetActive(aim);
+
+        if (aim)
+        {
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
+        }
+
+        thirdPersonController.SetSensitivity(aim ? aimSensitivity : normalSensitivity);
     }
 }
