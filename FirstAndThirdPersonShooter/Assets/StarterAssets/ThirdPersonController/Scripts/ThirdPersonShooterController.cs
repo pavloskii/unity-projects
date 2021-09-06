d61188d5b5dbf854739ef5076bdab1c5cbaffe1e
pavloskii <pavlo.igor@gmail.com>
2021-09-06 22:32:53 +0200

    Added pistol idel animation from mixamo.com

--- FirstAndThirdPersonShooter/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
+++ FirstAndThirdPersonShooter/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
@@ -17,32 +17,42 @@ public class ThirdPersonShooterController : MonoBehaviour
     private Transform bulletProjectile;
     [SerializeField]
     private Transform spawnBulletPosition;
+    [SerializeField]
+    public Transform vfxHitGreen;
+    [SerializeField]
+    public Transform vfxHitRed;
 
     private ThirdPersonController thirdPersonController;
     private StarterAssetsInputs starterAssetsInputs;
+    private Animator animator;
 
     private void Awake()
     {
         thirdPersonController = GetComponent<ThirdPersonController>();
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        Vector3 mousePosition = GetAimTargetPosition();
-        
-        ToggleAim(starterAssetsInputs.aim, mousePosition);
+        var targetTuple = GetAimTargetTransfromAndPosition();
+
+        ToggleAim(starterAssetsInputs.aim, targetTuple.Item2);
 
         if (starterAssetsInputs.shoot)
         {
-            Shoot(mousePosition);
+            Shoot(targetTuple.Item1);
         }
     }
 
-    private void Shoot(Vector3 mousePosition)
+    private void Shoot(Transform target)
     {
-        Vector3 aimDirection = (mousePosition - spawnBulletPosition.position).normalized;
-        Instantiate(bulletProjectile, spawnBulletPosition.position, Quaternion.LookRotation(aimDirection, Vector3.up));
+        if (target != null)
+        {
+            MarkTargetOnHit(target);
+        }
+        //Vector3 aimDirection = (mousePosition - spawnBulletPosition.position).normalized;
+        //Instantiate(bulletProjectile, spawnBulletPosition.position, Quaternion.LookRotation(aimDirection, Vector3.up));
         starterAssetsInputs.shoot = false;
     }
 
@@ -56,7 +66,9 @@ public class ThirdPersonShooterController : MonoBehaviour
             Vector3 aimDirection = (worldAimTarget - transform.position).normalized;
             transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
         }
-
+        
+        //set Aiming layer animation with index 1 to 1 or 0 depending if it is aiming or not
+        animator.SetLayerWeight(1, Mathf.Lerp(animator.GetLayerWeight(1), aim ? 1f : 0f, Time.deltaTime * 10f));
         //set aim camera to active (has priority over normal)
         aimVirtualCamera.gameObject.SetActive(aim);
         //Set different sensitivity in aim and normal
@@ -65,20 +77,28 @@ public class ThirdPersonShooterController : MonoBehaviour
         thirdPersonController.SetRotateOnMove(!aim);
     }
 
-    //Used for shooting projectile method
-    private Vector3 GetAimTargetPosition()
+    //Used for shooting hitscan method
+    private Tuple<Transform, Vector3> GetAimTargetTransfromAndPosition()
     {
-        //get center of the screen
+        Transform hitTransform = null;
+        Vector3 mouseWorldPosition = Vector3.zero;
+
         Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
-        
+
         if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
         {
-            return raycastHit.point;
+            hitTransform = raycastHit.transform;
+            mouseWorldPosition = raycastHit.point;
         }
-        //this can be used for not putting collider walls
-        //mouseWorldPosition = ray.GetPoint(10)
-        //return default of zero if a point can not be found
-        return Vector3.zero;
+
+        return new Tuple<Transform, Vector3>(hitTransform, mouseWorldPosition);
+    }
+
+    private void MarkTargetOnHit(Transform target)
+    {
+        var isHit = target.GetComponent<BulletTarget>() != null;
+
+        Instantiate(isHit ? vfxHitGreen : vfxHitRed, target.position, Quaternion.identity);
     }
 }
---
+++ FirstAndThirdPersonShooter/README.md
@@ -0,0 +1,8 @@
+# Third Person Shooter
+
+Built upon Code Monkey's [Awesome Third Person Shooter Controller](https://www.youtube.com/watch?v=FbM4CkqtOuA&t=1467s&ab_channel=CodeMonkey)
+
+All the Assets used for this project are free and can be found here: 
+1. [Starter Assets - Third Person Character Controller](https://assetstore.unity.com/packages/essentials/starter-assets-third-person-character-controller-196526)
+2. [POLYGON Starter Pack - Low Poly 3D Art by Synty](https://assetstore.unity.com/packages/3d/props/polygon-starter-pack-low-poly-3d-art-by-synty-156819)
+2. [Simple FX - Cartoon Particles](https://assetstore.unity.com/packages/vfx/particles/simple-fx-cartoon-particles-67834)
