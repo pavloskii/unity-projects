8fa92cdb4f0fc4196ae15b6954e29346f6f02aa7
pavloskii <pavlo.igor@gmail.com>
2021-09-09 22:01:48 +0200

    rename project

---
+++ SimplePlayerController/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(30, 0, 10);
+
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
